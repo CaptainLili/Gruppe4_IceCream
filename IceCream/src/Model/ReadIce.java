b19ca9c18f2a8c832c2fa85b4f3edba6ab3dd897
Eric <Eric@Eric-PC>
2015-11-30 20:13:17 +0100

    simpleView --> Implementierung des UI's ReadIce --> Änderungen zum Einlesen der CSV

--- IceCream/src/Model/ReadIce.java
+++ IceCream/src/Model/ReadIce.java
@@ -1,45 +1,57 @@
 package Model;
 
-import java.util.Calendar;
+import java.awt.List;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-
-import org.apache.commons.csv.CSVParser;
-
-
-import java.io.*;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class ReadIce {
-		float actual, variance, target;
-		String vColor, station;
-		Calendar date  = Calendar.getInstance();
-		File iceModel;
-		
-		public void readCSV(File iceModel){
-			CSVParser csvParser = new CSVParser( new FileInputStream("iceModel.csv") );
-			for ( String t; (t = csvParser.nextValue()) != null; )
-			  System.out.println( csvParser.lastLineNumber() + " " + t );
-			
-			
-			
-		}
-		public void writeCSV(String[] entries){
-		  CSVWriter writer = new CSVWriter(new FileWriter("iceModel.csv"), '\t');
-		     // feed in your array (or convert your data to an array)
-		     entries = "first#second#third".split("#");
-		     writer.writeNext(entries);
-			 writer.close();
-		
-		}
-		
-		public void getModel(){
-			readCSV(iceModel);
-		}
-		
-		
-		
-		
 
+	String vColor, station, actual, variance, target, date;
+
+	// float actual, variance, target;
+	// String vColor, station ;
+	// Calendar date = Calendar.getInstance();
+
+	public static void main(String[] args) {
+
+		BufferedReader br = null;
+		String line = "";
+
+		try {
+			br = new BufferedReader(new FileReader("iceModel.csv"));
+			while ((line = br.readLine()) != null) {
+				System.out.println("Converted ArrayList data: " + converted(line) + "\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null) br.close();
+			} catch (IOException brException) {
+				brException.printStackTrace();
+			}
+		}
 	}
-
-
+	
+	public static ArrayList<String> converted(String iceModelCSV) {
+		
+		ArrayList<String> result = new ArrayList<String>();
+		if (iceModelCSV != null) {
+			String[] splitData = iceModelCSV.split("\\s*,\\s*");
+			for (int i = 0; i < splitData.length; i++) {
+				if (!(splitData[i] == null) || !(splitData[i].length() == 0)) {
+					result.add(splitData[i].trim());
+				}
+			}
+		}
+		//System.out.println(result.get(2));
+		return result;
+	}
+	/*
+	 * public void getModel(){ readCSV(iceModel); }
+	 */
+}
--- IceCream/src/View/simpleView.java
+++ IceCream/src/View/simpleView.java
@@ -1,5 +1,108 @@
 package View;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.AbstractListModel;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+
+import Model.ReadIce;
+
 public class simpleView implements IceView{
 
+	public static void main(String[] args) {
+		// creates the JFrame(a window with decorations)
+		JFrame frame = new JFrame("IceCream Records");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+
+        JPanel label = new JPanel(new GridLayout(2, 0)); // the panel for the text on top
+        JPanel content = new JPanel(new GridLayout(2, 0)); // the main panel of the JFrame, remembet you cant add content directly to JFrame
+        JPanel splitPane = new JPanel(new GridLayout(0, 2, 20, 10));
+        JPanel listarea = new JPanel(new GridLayout(1, 0));
+        JPanel textarea = new JPanel(new GridLayout(5, 2, 2, 2)); // panel for the buttons, GridLayout(int rows, int columns, int horizontal_gap, int vertical_gap)
+        content.add(label);
+        content.add(splitPane);
+
+        //Label-->Title
+        JLabel title = new JLabel("Ice-Cream air-particle concentration.", JLabel.CENTER);
+        label.add(title);
+        
+        //splitPane-->listarea
+        JScrollPane scrollPane = new JScrollPane();
+        JList list = new JList();
+        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        list.setBorder(BorderFactory.createLineBorder(Color.black));
+        //placeholder for model
+        list.setModel(new AbstractListModel() {
+			String[] values = new String[] {
+					"AAAA", "AAAB", "AAAC", "AAAD", "AAAE", "AAAF",
+					"AABA", "AABB", "AABC", "AABD", "AABE", "AABF"
+					};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+        scrollPane.setViewportView(list);
+        listarea.add(scrollPane);
+        
+        //splitPane-->textarea
+        //StationID
+        JLabel stationID = new JLabel("StationID");
+        textarea.add(stationID);
+        JTextField station = new JTextField();
+        textarea.add(station);
+        
+        //Date
+        JLabel dateRecord = new JLabel("Date");
+        textarea.add(dateRecord);
+        JTextField dateEntry = new JTextField();
+        textarea.add(dateEntry);
+        
+        //Target
+        JLabel target = new JLabel("Target");
+        textarea.add(target);
+        JTextField targetEntry = new JTextField();
+        targetEntry.setText("placeholder");
+        targetEntry.setEditable(false);
+        textarea.add(targetEntry);
+        
+        //Actual
+        JLabel actual = new JLabel("Actual");
+        textarea.add(actual);
+        JTextField actualEntry = new JTextField();
+        textarea.add(actualEntry);
+
+        //Variance
+        JLabel variance = new JLabel("Variance");
+        textarea.add(variance);
+        JTextField varianceEntry = new JTextField();
+        textarea.add(varianceEntry);
+        
+        //Add
+        splitPane.add(listarea);
+        splitPane.add(textarea);
+        
+        //Borders
+        label.setBorder(BorderFactory.createLineBorder(Color.black));
+ 
+
+        frame.getContentPane().add(content); // adds the textarea panel to the main panel
+        frame.setVisible(true); // makes the window visible, put at end of program
+    }
+
+	@Override
+	public ReadIce getModel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
