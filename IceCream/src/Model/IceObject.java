19f3d92b3b59118a84247e27face1a804bba8b7e
CaptainLili <vanessavogelweb@web.de>
2015-11-30 11:52:18 +0100

    bearbeitung datenmodell

--- IceCream/src/Model/IceObject.java
+++ IceCream/src/Model/IceObject.java
@@ -1,5 +1,53 @@
 package Model;
 
+import java.util.Calendar;
+
 public class IceObject {
+	float actual, variance, target;
+	String vColor, station ;
+	Calendar date  = Calendar.getInstance();
+	
+	public float getActual() {
+		return actual;
+	}
+	public void setActual(float actual) {
+		this.actual = actual;
+	}
+	public float getVariance() {
+		return variance;
+	}
+	public void setVariance(float variance) {
+		this.variance = variance;
+	}
+	public float getTarget() {
+		return target;
+	}
+	public void setTarget(float target) {
+		this.target = target;
+	}
+	public String getvColor() {
+		return vColor;
+	}
+	public void setvColor(String vColor) {
+		this.vColor = vColor;
+	}
+	public String getStation() {
+		return station;
+	}
+	public void setStation(String station) {
+		this.station = station;
+	}
+	public Calendar getDate() {
+		return date;
+	}
+	public void setDate(Calendar date) {
+		this.date = date;
+	}
+	
+	public void changeObject(){
+		
+	}
+	
+	
 
 }
--- IceCream/src/Model/ReadIce.java
+++ IceCream/src/Model/ReadIce.java
@@ -1,20 +1,39 @@
 package Model;
 
 import java.util.Calendar;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.commons.csv.CSVParser;
 
 
 import java.io.*;
 
 public class ReadIce {
 		float actual, variance, target;
-		String vColor;
-		Calendar cal  = Calendar.getInstance();
+		String vColor, station;
+		Calendar date  = Calendar.getInstance();
+		File iceModel;
 		
 		public void readCSV(File iceModel){
+			CSVParser csvParser = new CSVParser( new FileInputStream("iceModel.csv") );
+			for ( String t; (t = csvParser.nextValue()) != null; )
+			  System.out.println( csvParser.lastLineNumber() + " " + t );
 			
 			
 			
-			
+		}
+		public void writeCSV(String[] entries){
+		  CSVWriter writer = new CSVWriter(new FileWriter("iceModel.csv"), '\t');
+		     // feed in your array (or convert your data to an array)
+		     entries = "first#second#third".split("#");
+		     writer.writeNext(entries);
+			 writer.close();
+		
+		}
+		
+		public void getModel(){
+			readCSV(iceModel);
 		}
 		
 		
